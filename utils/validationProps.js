4bf0f9cbe4390ce68f3161f7a7e96736d8386b6a
Sourav Gain <souravgain605@gmail.com>
2021-03-23 21:46:34 +0530

    added utility functions for routes & for other stuff

---
+++ utils/validationProps.js
@@ -0,0 +1,120 @@
+const {
+  string_prop,
+  boolean_prop,
+  object_prop,
+  valid_data,
+  number_prop,
+} = require("./validateData");
+const _addProps = [
+  string_prop("test_id"),
+  string_prop("title"),
+  boolean_prop("mcq"),
+  boolean_prop("saq"),
+  boolean_prop("baq"),
+  string_prop("ans"),
+  string_prop("note"),
+  object_prop("options"),
+];
+
+const _addManagerProps = [
+  string_prop("name"),
+  string_prop("username"),
+  string_prop("password"),
+];
+
+const _addStudentProps = [
+  string_prop("name"),
+  string_prop("username"),
+  string_prop("password"),
+  string_prop("email"),
+  string_prop("roll"),
+  number_prop("semester"),
+  object_prop("department"),
+  object_prop("batch"),
+];
+
+const _addFacultyProps = _addManagerProps;
+
+const _addDepartmentProps = [
+  string_prop("name"),
+  string_prop("short_name"),
+  number_prop("semesters"),
+];
+
+const _addBatchProps = [string_prop("name")];
+
+const _addSubjectProps = [string_prop("name"), string_prop("paper_code")];
+
+const _addTestProps = [
+  string_prop("name"),
+  string_prop("in_charge"),
+  string_prop("department"),
+  string_prop("batch"),
+  string_prop("subject"),
+  number_prop("semester"),
+  string_prop("start_time"),
+  number_prop("start_time_offset"),
+  number_prop("end_time_restrict"),
+  number_prop("full_time"),
+  boolean_prop("suffle"),
+  boolean_prop("negative_marking"),
+  number_prop("negative_value"),
+  number_prop("negative_threshold"),
+
+  object_prop("groups"),
+  // object_prop("questions"),
+  // object_prop("answers")
+];
+
+const _removeTestProps = [string_prop("id"), string_prop("in_charge")];
+
+const _addQuestionProps = [
+  string_prop("title"),
+  string_prop("note"),
+  boolean_prop("mcq"),
+  boolean_prop("saq"),
+  boolean_prop("baq"),
+  number_prop("mark"),
+  object_prop("options"),
+];
+
+const _middleware_addManager = (req, res, next) => {
+  if (valid_data(req.body, _addManagerProps)) {
+    next();
+  } else {
+    res.status(400).json({ msg: "Invalid Data" });
+  }
+};
+
+const _middleware_addStudent = (req, res, next) => {
+  if (valid_data(req.body, _addStudentProps)) {
+    next();
+  } else {
+    res.status(400).json({ msg: "Invalid Data" });
+  }
+};
+
+const _middleware_addFaculty = (req, res, next) => {
+  if (
+    valid_data(req.query, _addFacultyProps) ||
+    valid_data(req.body, _addFacultyProps)
+  ) {
+    next();
+  } else {
+    res.status(400).json({ msg: "Invalid Data" });
+  }
+};
+
+module.exports = {
+  _addProps,
+  _addManagerProps,
+  _addDepartmentProps,
+  _addBatchProps,
+  _addSubjectProps,
+  _addTestProps,
+  _removeTestProps,
+  _addQuestionProps,
+  _middleware_addManager,
+  _middleware_addStudent,
+  _middleware_addFaculty,
+};
