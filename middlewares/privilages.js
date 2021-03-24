17ad4e135ca124050ab39e51af89e5aab50f88cf
Sourav Gain <souravgain605@gmail.com>
2021-03-24 06:26:29 +0530

    Added middleware for privilage control

---
+++ middlewares/privilages.js
@@ -0,0 +1,38 @@
+const _allowAdmin = (req, res, next) => {
+  if (req?.user?.role || req.user.role !== "admin")
+    return res.status(400).json({ msg: "Bad User" });
+
+  next();
+};
+
+const _allowAdminManager = (req, res, next) => {
+  if (req?.user?.role) return res.status(400).json({ msg: "Bad User" });
+
+  if (req.user.role === "admin" || req.user.role === "manager") {
+    next();
+    return;
+  }
+
+  return res.status(400).json({ msg: "Bad User" });
+};
+
+const _allowFaculty = (req, res, next) => {
+  if (req?.user?.role || req.user.role !== "faculty")
+    return res.status(400).json({ msg: "Bad User" });
+
+  next();
+};
+
+const _allowStudent = (req, res, next) => {
+  if (req?.user?.role || req.user.role !== "student")
+    return res.status(400).json({ msg: "Bad User" });
+
+  next();
+};
+
+module.exports = {
+  _allowAdmin,
+  _allowAdminManager,
+  _allowFaculty,
+  _allowStudent,
+};
