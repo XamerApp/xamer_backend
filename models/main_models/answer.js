432ffc05a3ca6349ff36ee40232520ee2692e3d5
Sourav Gain <souravgain605@gmail.com>
2021-04-02 17:48:49 +0530

    Changed structure of answer schema

--- models/main_models/answer.js
+++ models/main_models/answer.js
@@ -3,22 +3,22 @@ const {
   ANSWER_COLLECTION,
   STUDENT_COLLECTION,
   QUESTION_COLLECTION,
+  TEST_COLLECTION,
 } = process.env;
 
 const answer_block_schema = Schema({
   question: { type: SchemaTypes.ObjectId, ref: QUESTION_COLLECTION },
   selection: { type: SchemaTypes.Number, default: null },
   text: { type: SchemaTypes.String, default: "" },
-});
-
-const answer_list_schema = Schema({
-  student: { type: SchemaTypes.ObjectId, ref: STUDENT_COLLECTION },
-  answers: [answer_block_schema],
+  visited: { type: SchemaTypes.Boolean, default: false },
+  answered: { type: SchemaTypes.Boolean, default: false },
+  marked: { type: SchemaTypes.Boolean, default: false },
 });
 
 const answer = Schema({
-  test_id: SchemaTypes.String,
-  answer_list: [answer_list_schema],
+  test_id: { type: SchemaTypes.ObjectId, ref: TEST_COLLECTION },
+  student: { type: SchemaTypes.ObjectId, ref: STUDENT_COLLECTION },
+  answers: [answer_block_schema],
 });
 
 module.exports = model(ANSWER_COLLECTION, answer);
