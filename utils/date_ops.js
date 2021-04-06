ec8ed8da8caa4087a43a64649b6723733cfdced9
Sourav Gain <souravgain605@gmail.com>
2021-04-06 23:07:52 +0530

    added more functions for date calculations

--- utils/date_ops.js
+++ utils/date_ops.js
@@ -14,7 +14,49 @@ const is_eligible_by_hour = (start_date, full_time) => {
   return current > start && current < end ? true : false;
 };
 
+const time_left_in_min = (end_time) => {
+  const current = new Date().getTime();
+  const end = end_time.getTime();
+  const left = end - current;
+  if (left <= 0) return 0;
+  return left / (1000 * 60);
+};
+
+const time_left_in_second = (end_time) => {
+  const current = new Date().getTime();
+  const end = end_time.getTime();
+  const left = end - current;
+  if (left <= 0) return 0;
+  return left / 1000;
+};
+
+const time_left_in_hour = (end_time) => {
+  const current = new Date().getTime();
+  const end = end_time.getTime();
+  const left = end - current;
+  if (left <= 0) return 0;
+  return left / (1000 * 3600);
+};
+
+const is_eligible_to_create_test = (start_time, offset) => {
+  const current = new Date();
+  const start = start_time.getTime();
+  const offset_in_ms = offset * 3600000;
+
+  // We cannnot create a test which will be taken after 9 mins from the start offset
+  // because thats not propertime for creating test
+  const constantOffset = 600000; // 10 mins in ms
+
+  const restrict = start - offset_in_ms - constantOffset;
+  if (restrict <= 0) return false;
+  return current.getTime() < restrict ? true : false;
+};
+
 module.exports = {
   is_eligible_by_hour,
   is_eligible_by_min,
+  time_left_in_hour,
+  time_left_in_min,
+  time_left_in_second,
+  is_eligible_to_create_test,
 };
