260e488d9f64ea4551331acd07c6b7050489fb51
Sourav Gain <souravgain605@gmail.com>
2021-04-07 19:45:21 +0530

    added options useFindAndModify as false to remove deprecated warning

--- auth_server.js
+++ auth_server.js
@@ -23,6 +23,7 @@ mongoose
   .connect(process.env.DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false,
   })
   .then(() => {
     console.log("Database connected :)");
