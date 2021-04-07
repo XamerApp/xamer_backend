8bdc7cd3af69a9d5f235b7ed605ea2b7b5b1e988
Sourav Gain <souravgain605@gmail.com>
2021-04-07 19:44:16 +0530

    added utility function to check if student can continue exam

--- utils/date_ops.js
+++ utils/date_ops.js
@@ -62,6 +62,16 @@ const is_eligible_to_start_exam = (start_time, full_time, offset) => {
   return current > start && current < end ? true : false;
 };
 
+const is_eligible_to_continue_exam = (start_time, full_time) => {
+  const start = start_time.getTime();
+  const end = start_time.getTime() + full_time * 60000;
+  const current = new Date().getTime();
+  // console.log(new Date(start).toLocaleString());
+  // console.log(new Date(end).toLocaleString());
+  // console.log(new Date(current).toLocaleString());
+  return current > start && current < end ? true : false;
+};
+
 module.exports = {
   is_eligible_by_hour,
   is_eligible_by_min,
@@ -70,4 +80,5 @@ module.exports = {
   time_left_in_second,
   is_eligible_to_create_test,
   is_eligible_to_start_exam,
+  is_eligible_to_continue_exam,
 };
